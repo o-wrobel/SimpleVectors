151c9838ca0f2e6832cf4fe09bd2f72b96a5b6ca
Oskar Wróbel <o.wrobel2025@protonmail.com>
2026-03-21 07:44:18 +0100

    Added a second module to house Vector3

--- build.zig
+++ build.zig
@@ -2,9 +2,15 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    _ = b.addModule("Vector3", .{
+    const vector3 = b.createModule(.{
+	    .root_source_file = b.path("src/Vector3.zig")
+    });
+
+    const mod = b.addModule("simplevectors", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
     });
+
+    mod.addImport("Vector3", vector3);
 
 }

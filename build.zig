0aff01a1cfed800e8cdd6af2e3a8450f02a6ee85
Oskar Wróbel <o.wrobel2025@protonmail.com>
2026-05-11 17:05:08 +0200

    Changed createModule to addModule

--- build.zig
+++ build.zig
@@ -3,14 +3,20 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
 	const target = b.standardTargetOptions(.{});
 	const optimize = b.standardOptimizeOption(.{});
-	_ = b.createModule(.{
-		.root_source_file = b.path("src/Vector3.zig"),
-		.target = target,
-		.optimize = optimize,
-	});
-	_ = b.createModule(.{
-		.root_source_file = b.path("src/Vector2.zig"),
-		.target = target,
-		.optimize = optimize,
-	});
+	_ = b.addModule(
+		"Vector2",
+		.{
+			.root_source_file = b.path("src/Vector2.zig"),
+			.target = target,
+			.optimize = optimize,
+		}
+	);
+	_ = b.addModule(
+		"Vector3",
+		.{
+			.root_source_file = b.path("src/Vector3.zig"),
+			.target = target,
+			.optimize = optimize,
+		}
+	);
 }
